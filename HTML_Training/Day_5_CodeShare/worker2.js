d38506c123f136a8de18aed75040dec989625390
chandra-79 <chandra@ajnacs.com>
2020-11-08 22:34:24 +0800

    Cleanup

---
+++ HTML_Training/Day_5_CodeShare/worker2.js
@@ -0,0 +1,11 @@
+﻿onmessage = function (e) {
+    console.log('Worker2: Message received from main script');
+    const result = e.data[0] + e.data[1];
+    if (isNaN(result)) {
+        postMessage('Please write two numbers');
+    } else {
+        const workerResult = 'Result: ' + result;
+        console.log('Worker2: Posting message back to main script');
+        postMessage(workerResult);
+        self.close();}
+}
